280d7d4486030a3ceab545c204cf2ca92a0da764
Marcello Azevedo <imcello@hotmail.com>
2022-05-12 11:16:48 -0300

    video firebase finish

---
+++ build_firebase/precache-manifest.27bdfb0134819f53ef0483aed69034f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc31aa5e4514c293bd7",
+    "url": "/static/css/main.3558002e.chunk.css"
+  },
+  {
+    "revision": "7cc31aa5e4514c293bd7",
+    "url": "/static/js/main.7cc31aa5.chunk.js"
+  },
+  {
+    "revision": "cf9e4a10b582dd373ed5",
+    "url": "/static/js/1.cf9e4a10.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e60338c11e94c5a256aed4fdd07d6715",
+    "url": "/static/media/pic.e60338c1.jpg"
+  },
+  {
+    "revision": "3dcd742ddae2a2534173f52305d05d00",
+    "url": "/index.html"
+  }
+];
